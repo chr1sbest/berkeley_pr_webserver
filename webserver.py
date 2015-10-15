c15b3878f069ddacc5563ec0c719cbcb38474df8
Chris Best <chr2sbest@gmail.com>
2015-10-14 19:10:49 -0700

    Updates to webserver.py to find mock data relative to webserver.py

--- webserver.py
+++ webserver.py
@@ -1,28 +1,33 @@
+import os
 import json
 from flask import Flask, jsonify, render_template
+
+# Initialize flask application
 application = Flask(__name__,
                     template_folder='frontend',
                     static_folder='frontend')
+
+# Determine `pwd` of this executing file
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @application.route("/")
 def hello():
     return render_template('index.html')
 
-
 @application.route("/rankings")
 def rankings():
-    with open('mock_data/ranking_mock.json', 'r') as data:
+    path = os.path.join(CURRENT_DIR, 'mock_data/ranking_mock.json')
+    with open(path, 'r') as data:
         ranking_data = json.load(data)
     return jsonify(**ranking_data)
 
-
 @application.route("/players/<player_id>")
 def player(player_id):
-    player_file = "mock_data/player_{}_mock.json".format(player_id)
-    with open(player_file, 'r') as data:
+    path = os.path.join(CURRENT_DIR,
+                        "mock_data/player_{}_mock.json".format(player_id))
+    with open(path, 'r') as data:
         player_data = json.load(data)
     return jsonify(**player_data)
-
 
 if __name__ == "__main__":
     application.run()
