39948c44e4f58337481b7194dd8310b3777a7758
Charles Lee <charleslee94@berkeley.edu>
2015-10-15 21:13:07 -0700

    Separated into columns, still cant get them into same rows

--- frontend/static/js/templates.js
+++ frontend/static/js/templates.js
@@ -2,7 +2,10 @@
 Handlebars.registerHelper('list', function(items, options) {
   var out = "<ol style='text-align:center'>";
   for(var i=0, l=items.length; i<l; i++) {
-    out = out + "<li>" + options.fn(items[i]) + "</li>";
+    var str = (options.fn(items[i]).split("--"));
+    var a = str[0];
+    var b = str[1];
+    out = out + a +  "<li style ='text-align:right'>" + b + "</li>";
   }
   return out + "</ol>";
 });
