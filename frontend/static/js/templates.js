caf43bf0369c2677116070c8a04212e06ee21ba9
Brian Choi <brianchoi_1994@hotmail.com>
2015-10-16 04:48:45 -0700

    formatted

--- frontend/static/js/templates.js
+++ frontend/static/js/templates.js
@@ -1,12 +1,18 @@
 // List helper
 Handlebars.registerHelper('list', function(items, options) {
-  var out = "<ul style='list-style-type:none'>";
+  var out = "<ul class='main',style='list-style-type:none'>" + "<ul class='element'>" + "<li class='header rank format'>" + "Rank" + "</li>" + "<li class='header player format'>" + "Player" + "</li>" + "<li class='header rating format'>" + "Rating" + "</li>" + "</ul>" + "<br>";
   for(var i=0, l=items.length; i<l; i++) {
     var str = (options.fn(items[i]).split("--"));
 	var a = i + 1	    
 	var b = str[0];
-    	var c = str[1];
-    out = out + "<ul class='element'>" + "<li class='rank format'>" + a + "</li>" + "<li class='name format'>" + b + "</li>" + "<li class='rating format'>" + c + "</li>" + "</ul>" + "<br>";
- }
+    	var c = str[1].substring(0,7);
+// alternating color per element
+	if (i%2) {
+	out = out + "<ul class='element'>" + "<li class='light rank format'>" + a + "</li>" + "<li class='light player format'>" + b + "</li>" + "<li class='light rating format'>" + c + "</li>" + "</ul>" + "<br>";
+	}
+	else {
+	out = out + "<ul class='element'>" + "<li class='dark rank format'>" + a + "</li>" + "<li class='dark player format'>" + b + "</li>" + "<li class='dark rating format'>" + c + "</li>" + "</ul>" + "<br>";
+} 
+}
   return out + "</ol>";
 });
