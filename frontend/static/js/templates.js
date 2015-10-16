5418e844b2f267e4ba149657ab52cff535f70183
Brian Choi <brianchoi_1994@hotmail.com>
2015-10-15 21:33:06 -0700

    checkpoint commit: separated ranking info into separate columns

--- frontend/static/js/templates.js
+++ frontend/static/js/templates.js
@@ -1,8 +1,12 @@
 // List helper
 Handlebars.registerHelper('list', function(items, options) {
-  var out = "<ol style='text-align:center'>";
+  var out = "<ul style='list-style-type:none'>";
   for(var i=0, l=items.length; i<l; i++) {
-    out = out + "<li>" + options.fn(items[i]) + "</li>";
+    var str = (options.fn(items[i]).split("--"));
+	var a = i + 1	    
+	var b = str[0];
+    	var c = str[1];
+    out = out + "<ul class='item'>" + "<li class='rank'>" + a + "</li>" + "<li class='name'>" + b + "</li>" + "<li class='rating'>" + c + "</li>" + "</ul>" + "<br>";
   }
   return out + "</ol>";
 });
