af21f69c8cb85c794b41a8f41742c37e52fcc831
Chris Best <chr2sbest@gmail.com>
2015-10-16 19:38:33 -0700

    Added semicolon to missing line

--- frontend/static/js/templates.js
+++ frontend/static/js/templates.js
@@ -3,9 +3,9 @@ Handlebars.registerHelper('list', function(items, options) {
   var out = "<ul style='list-style-type:none'>";
   for(var i=0, l=items.length; i<l; i++) {
     var str = (options.fn(items[i]).split("--"));
-	var a = i + 1	    
-	var b = str[0];
-    	var c = str[1];
+    var a = i + 1;
+    var b = str[0];
+    var c = str[1];
     out = out + "<ul class='element'>" + "<li class='rank format'>" + a + "</li>" + "<li class='name format'>" + b + "</li>" + "<li class='rating format'>" + c + "</li>" + "</ul>" + "<br>";
  }
   return out + "</ol>";
