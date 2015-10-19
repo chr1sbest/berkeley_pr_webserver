0664cc0a8a9109ac5ca5631431501a0008da44bd
Chris Best <chr2sbest@gmail.com>
2015-10-18 18:39:15 -0700

    Successfully linked the playersData to our templates in our Players page. Built custom handlebars helper to take care of some of our data.

--- frontend/static/js/templates.js
+++ frontend/static/js/templates.js
@@ -1,12 +1,36 @@
 // List helper
 Handlebars.registerHelper('list', function(items, options) {
-  var out = "<table class='table table-striped'>" + "<thead>" + "<tr>" + "<th>Rank</th>" + "<th>Player</th>" + "<th>Rating</th>" + "</tr>" + "</thead>" + "<tbody>";
+  var out = '';
   for(var i=0, l=items.length; i<l; i++) {
-        var item = items[i];
-	var ranking = i + 1;	    
-	var player = item.player;
-    	var rating = Math.round(item.rating * 1000) / 1000/*toString().substring(0,6);*/
-	var out = out + "<tr>" + "<td>" + ranking + "</td>" + "<td>" + player + "</td>" + "<td>" + rating + "<td>" + "</tr>";
+    var item = items[i];
+    var ranking = i + 1;	    
+    var player = item.player;
+    var rating = Math.round(item.rating * 1000) / 1000/*toString().substring(0,6);*/
+
+    var out = out + "<tr>" + "<td>" + ranking + "</td>" + "<td>" + player + "</td>" + "<td>" + rating + "<td>" + "</tr>";
   }
-  return out + "</tbody>" + "</table>";
+  return out;
+});
+
+
+Handlebars.registerHelper('playerMatches', function(items, options) {
+  console.log(items);
+  var out = '';
+  var player_data  = items.data.root;
+  var matches = player_data['matches'];
+  _.each(matches, function(match){
+    var date = '10/15/2015';
+    var opp = match['opp'];
+
+    // CHANGE THIS LATER CHARLES
+    var outcome = match['outcome'];
+    if (outcome == "lose"){
+      outcome = "loss";
+    }
+
+    var tournament = match['tournament'];
+    out = out + "<tr class='" + outcome + "'>" + "<td>" + date + "</td>" + "<td><a href='/#players/" + opp + "'>" + opp + "</a></td>" + "<td>" + outcome + "</td>" + "<td>" + tournament + "<td>" + "</tr>";
+
+  });
+  return out
 });
