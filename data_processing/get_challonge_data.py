b3a9d23609f4304f8c210ec40ea31ea4c10d4738
Chris Best <chr2sbest@gmail.com>
2015-12-28 16:19:00 -0800

    cleaned up get_challonge_data

--- data_processing/get_challonge_data.py
+++ data_processing/get_challonge_data.py
@@ -1,9 +1,11 @@
-import json
+"""
+Query Challonge for tournament data and upsert to Mongo.
+"""
+import pymongo
 from requests import get
 from config import CHALLONGE_KEY
-import pymongo
 
-client = pymongo.MongoClient()
+CLIENT = pymongo.MongoClient()
 
 def get_tournament_data(tournament_id):
     """ Query challonge API for player and match details.
@@ -23,6 +25,7 @@ def get_tournament_data(tournament_id):
         'created_at': players[-1]['created_at']
     }
 
-def upload_tournament_data(data, client=client):
+def upload_tournament_data(data, client=CLIENT):
+    """ Insert tournament data into Mongo."""
     tournaments_collection = client['production']['tournaments']
     tournaments_collection.insert(data)
