0845ccf64d85eac3949eee72caf4651cf303f69d
Charles Lee <charleslee94@berkeley.edu>
2016-01-02 17:08:42 -0800

    added test for build user matches list

--- data_processing/nightly_processing_tests.py
+++ data_processing/nightly_processing_tests.py
@@ -31,7 +31,9 @@ class TestNightlyProcessing(unittest.TestCase):
             update_registered = update_registered_user_matches(updated, user)
         self.assertTrue(client['test']['users'].find({'facebook_id': 1}))
 
+
     def test_update_player_trueskill(self):
+
         """ Pull data from 'sab-gb3champ' bracket, with 25 participants
         for each match of the tournament, pick the winner and loser
         update winner/loser rating with winner/loser trueskill rating
@@ -47,6 +49,35 @@ class TestNightlyProcessing(unittest.TestCase):
         lowest = ratings_map.get(31900062)
         self.assertEquals(highest, max(ratings_map.values()))
         self.assertEquals(lowest, min(ratings_map.values()))
+
+
+    def test_build_user_matches_list(self):
+        """ using gb3champ, build list of matches for each player"""
+        tournament = get_tournament_data('sab-gb3champ')
+        player_matches = defaultdict(list)
+        player_matches = build_user_matches_list(tournament, player_matches)
+        # test that the match # of the top 3 lines up to the standings on sab.challonge.com/gb3champ
+        first = player_matches[31900057]
+        second = player_matches[31900058]
+        third = player_matches[31900059]
+        self.assertEquals(len(first), 5)
+        self.assertEquals(len(second), 6)
+        self.assertEquals(len(third), 7)
+        # test that all matches for player have their id in wins or losses, first place has all wins in
+        # this particular instance
+        for match in first:
+            self.assertEquals(match['winner_id'], 31900057)
+        for match in second:
+            isEq = match['winner_id'] == 31900058 or match['loser_id'] == 31900058
+            self.assertTrue(isEq)
+        for match in third:
+            isEq = match['winner_id'] == 31900059 or match['loser_id'] == 31900059
+            self.assertTrue(isEq)
+
+        
+
+
+
         
 
         
